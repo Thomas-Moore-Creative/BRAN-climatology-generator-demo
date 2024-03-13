1711be97d99b3982904c094d4b08966918f30bdb
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-03-13 13:59:55 +1100

    change queue to hugmem and jobfs to 1400GB #2

--- src/scripts/make_BRAN2020_clim_ARD.sh
+++ src/scripts/make_BRAN2020_clim_ARD.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
 #PBS -P es60
-#PBS -q megamem
-#PBS -l walltime=10:00:00
+#PBS -q hugmem
+#PBS -l walltime=24:00:00
 #PBS -l ncpus=48
-#PBS -l mem=3000GB
-#PBS -l jobfs=800GB
+#PBS -l mem=1470GB
+#PBS -l jobfs=1400GB
 #PBS -l wd
 #PBS -l storage=gdata/gb6+gdata/v14+gdata/es60+scratch/es60+gdata/al33+gdata/cj50+gdata/dk92+gdata/fs38+gdata/ik11+gdata/oi10+gdata/p73+gdata/rr3+gdata/xp65
 #PBS -j oe
