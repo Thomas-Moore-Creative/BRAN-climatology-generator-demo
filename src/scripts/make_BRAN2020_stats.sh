95496df0ac35cf7fd95899e35a34931b5725ff33
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-03-15 20:04:59 +1100

    added xv83 to fix bug #2

--- src/scripts/make_BRAN2020_stats.sh
+++ src/scripts/make_BRAN2020_stats.sh
@@ -7,7 +7,7 @@
 #PBS -l mem=2990GB
 #PBS -l jobfs=1400GB
 #PBS -l wd
-#PBS -l storage=gdata/gb6+gdata/v14+gdata/es60+scratch/es60+gdata/al33+gdata/cj50+gdata/dk92+gdata/fs38+gdata/ik11+gdata/oi10+gdata/p73+gdata/rr3+gdata/xp65
+#PBS -l storage=gdata/gb6+gdata/v14+gdata/es60+scratch/es60+gdata/al33+gdata/cj50+gdata/dk92+gdata/fs38+gdata/ik11+gdata/oi10+gdata/p73+gdata/rr3+gdata/xp65+gdata/xv83
 #PBS -j oe
 
 conda activate pangeo_regionmask
