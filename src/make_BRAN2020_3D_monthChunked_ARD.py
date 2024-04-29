49492564ee55b5dbf0c27f672a86d5444cf729de
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-04-29 16:53:32 +1000

    update before archive

--- src/make_BRAN2020_3D_monthChunked_ARD.py
+++ src/make_BRAN2020_3D_monthChunked_ARD.py
@@ -24,7 +24,7 @@ def main():
     print(client)
     # -----------  setup ----------------------
     print("setup")
-    var_request_list = ['salt','u','v']
+    var_request_list = ['u','v']
     time_period_request_list = ['daily']
     # ----- NRI Catalog ---
     print("opening BRAN2020 intake catalog")
@@ -53,14 +53,16 @@ def main():
         print("load DS")
         if var in ['mld','eta_t']:
             xarray_open_kwargs = {"chunks": {"time": 27,  "xt_ocean": 3600, "yt_ocean": 1500}}
+        elif var in ['u','v']: 
+            xarray_open_kwargs = {"chunks": {"time": 27, "st_ocean": 10, "xu_ocean": 3600, "yu_ocean": 1500}}
         else: 
-            xarray_open_kwargs = {"chunks": {"time": 27, "st_ocean": 10, "xt_ocean": 3600, "yt_ocean": 1500}}
+            xarray_open_kwargs = {"chunks": {"time": 27, "st_ocean": 10, "xt_ocean": 3600, "yt_ocean": 1500}}   
         DS=search.to_dask(xarray_open_kwargs=xarray_open_kwargs)
         # ARD - write zarr & chunk & write zarr
         print(var+" ARD - start write first zarr")
         BRAN2020_ard_path = '/scratch/es60/ard/reanalysis/BRAN2020/ARD/'
-        chunking_string = 'chunks_'+''.join(str(key) + str(value) for key, value in xarray_open_kwargs['chunks'].items())
-        ard_file_ID = 'BRAN2020-daily-'+var+'-'+chunking_string+'-v25042024.zarr'
+        chunking_string = 'chunks.' + ''.join(str(key) + str(value) + '.' for key, value in xarray_open_kwargs['chunks'].items())
+        ard_file_ID = 'BRAN2020.daily.'+var+'.'+chunking_string+'v26042024.zarr'
         DS[var]=DS[var].astype('float32')
         DS.to_zarr(BRAN2020_ard_path+ard_file_ID,consolidated=True)
         print(var+" ARD - finished monthChunked zarr collection for "+var)
