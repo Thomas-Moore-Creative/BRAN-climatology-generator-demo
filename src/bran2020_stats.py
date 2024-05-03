cb9231e0af98be1b117831e0ebfd421b538124de
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-05-03 14:52:57 +1000

    updated stats functions to reflect flox testing #17

--- src/bran2020_stats.py
+++ src/bran2020_stats.py
@@ -4,34 +4,45 @@
 Module Name: bran2020_stats.py
 Description: This module provides statistical functions for analyzing BRAN2020 data.
 Author: 
-Date: 24 April 2024
+Date: 3 May 2024
 """
 
 # Import statements
 import xarray as xr
 import numpy as np
-from flox.xarray import xarray_reduce
 import flox
-import flox.xarray
+import bottleneck
+import numba
+import numbagg
+
 
 # Function definitions
-def mean_monthclim_flox(ds,var_name,time_dim='time',method='map-reduce',skipna=False):
-    if skipna == False:
-        mean = xarray_reduce(ds, time_dim+'.month', func="mean",method=method).rename({var_name:'mean_'+var_name})
-        std = xarray_reduce(ds, time_dim+'.month', func="std",method=method).rename({var_name:'std_'+var_name})
+def mean_monthclim_flox(ds,var_name,time_dim='time',method_str='cohorts',skipna_flag=False):
+    """
+    currently written for single variable datasets
+    """
+    if skipna_flag == False:
+        mean = ds.groupby(time_dim+'.month').mean(dim=time_dim,engine='flox',method=method_str,skipna=False).rename({var_name:'mean_'+var_name})
+        std = ds.groupby(time_dim+'.month').std(dim=time_dim,engine='flox',method=method_str,skipna=False).rename({var_name:'std_'+var_name})
     else:
-        mean = xarray_reduce(ds, time_dim+'.month', func="nanmean",method=method).rename({var_name:'mean_'+var_name})
-        std = xarray_reduce(ds, time_dim+'.month', func="nanstd",method=method).rename({var_name:'std_'+var_name})
-    max = xarray_reduce(ds, time_dim+'.month', func="argmax",method=method).rename({var_name:'max_'+var_name})
-    min = xarray_reduce(ds, time_dim+'.month', func="argmin",method=method).rename({var_name:'min_'+var_name})
+        mean = ds.groupby(time_dim+'.month').mean(dim=time_dim,engine='numbagg').rename({var_name:'mean_'+var_name})
+        std = ds.groupby(time_dim+'.month').std(dim=time_dim,engine='numbagg').rename({var_name:'std_'+var_name})
+    max = ds.groupby(time_dim+'.month').max(dim=time_dim,engine='flox',method=method_str).rename({var_name:'max_'+var_name})
+    min = ds.groupby(time_dim+'.month').min(dim=time_dim,engine='flox',method=method_str).rename({var_name:'min_'+var_name})
     stats_ds = xr.merge([mean,std,max,min])
     return stats_ds
 
 def median_monthclim(ds,var_name,skipna_flag=False,time_dim='time'):
-    median_ds = ds.groupby(time_dim+'.month').median(skipna=skipna_flag).rename({var_name:'median_'+var_name})
+    """
+    current practice is to provide a {'time':-1} chunked ds for median calculation
+    """
+    median_ds = ds.groupby(time_dim+'.month').median(skipna=skipna_flag,engine='flox').rename({var_name:'median_'+var_name})
     return median_ds
 
 def quantile_monthclim(ds,var_name,skipna_flag=False,time_dim='time',q_list=[0.05,0.95]):
-    quant = ds.groupby(time_dim+'.month').quantile(q_list,skipna=skipna_flag,dim=time_dim).astype(np.float32)
+    """
+    current practice is to provide a {'time':-1} chunked ds for quantile calculation
+    """
+    quant = ds.groupby(time_dim+'.month').quantile(q_list,skipna=skipna_flag,dim=time_dim,engine='flox').astype(np.float32)
     quant_ds = xr.merge([quant.isel(quantile=0).reset_coords(drop=True).rename({var_name:'quantile_05_'+var_name}),quant.isel(quantile=1).reset_coords(drop=True).rename({var_name:'quantile_95_'+var_name})])
     return quant_ds
